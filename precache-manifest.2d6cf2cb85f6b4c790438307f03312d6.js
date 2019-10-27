9234b172c89223368514fecd50bd1cf4922c417e
Alexander Dolgov <sanhoxd@gmail.com>
2019-10-27 17:33:35 +0300

    Updates

---
+++ precache-manifest.2d6cf2cb85f6b4c790438307f03312d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e242682998f162ccc1d7564bd3a23882",
+    "url": "/pokeAPI/index.html"
+  },
+  {
+    "revision": "289aa0f861e80a20909b",
+    "url": "/pokeAPI/static/css/main.4d18a5b3.chunk.css"
+  },
+  {
+    "revision": "1920bf96b3911ab17a31",
+    "url": "/pokeAPI/static/js/2.58847b0e.chunk.js"
+  },
+  {
+    "revision": "289aa0f861e80a20909b",
+    "url": "/pokeAPI/static/js/main.2d69dda6.chunk.js"
+  },
+  {
+    "revision": "c46122b0f35ad85d29e4",
+    "url": "/pokeAPI/static/js/runtime-main.0f837a3c.js"
+  }
+]);
